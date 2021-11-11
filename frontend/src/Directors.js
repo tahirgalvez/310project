2c9049ef102029676d707c0559a1d15bc6d3de27
tahirgalvez <61433382+tahirgalvez@users.noreply.github.com>
2021-11-10 19:50:12 -0600

    added routes and navbar

---
+++ frontend/src/Directors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+class Directors extends React.Component {
+
+    render() {
+        return (
+            <Container align='center'>
+                Directors
+            </Container>
+        )
+    }
+}
+
+export default Directors;
