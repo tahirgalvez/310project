9583aa317ffbe921fffa9db8d6a3ddd7ebd0769d
tahirgalvez <61433382+tahirgalvez@users.noreply.github.com>
2021-11-15 15:34:49 -0600

    changed tabs added sample search table for movies

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -4,7 +4,6 @@ import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Homepage  from './Homepage';
 import Actors from './Actors';
 import Movies from './Movies';
 import Directors from './Directors';
@@ -13,9 +12,8 @@ function NavigationBar() {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Home</Navbar.Brand>
+                <Navbar.Brand href="/movies">Movies</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="/movies">Movies</Nav.Link>
                     <Nav.Link href="/actors">Actors</Nav.Link>
                     <Nav.Link href="/directors">Directors</Nav.Link>
                 </Nav>
@@ -34,11 +32,11 @@ class Navvv extends React.Component {
                     {NavigationBar()}
                     <Container title="cont">
                         <Switch title="switch">
-                            <Route exact path="/" component={Homepage} />
+                            <Route exact path="/" component={Movies} />
                             <Route exact path="/movies" component={Movies} />
                             <Route exact path="/actors" component={Actors} />
                             <Route exact path="/directors" component={Directors} />
-                            <Route component={Homepage} />
+                            <Route component={Movies} />
                         </Switch>
                     </Container>
                 </Router>
