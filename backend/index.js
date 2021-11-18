5266696ff379d6b4c6f8669040e73c29da7d51d5
tahirgalvez <61433382+tahirgalvez@users.noreply.github.com>
2021-11-18 11:12:37 -0600

    fixed routes

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get('/shows', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')));
+app.get('/crewcast', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')));
 app.get('/movies', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')));
-app.get('/actors', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')));
-app.get('/directors', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')));
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
