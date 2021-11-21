7e30acd2f1cd38a6bb8da70807c39414b03aa3a9
julieherrick <julieherrick@tamu.edu>
2021-11-20 20:57:39 -0600

    row handler working
    Alert happens when you click row, need to make the row handler link to the popup though

--- frontend/src/Movies.js
+++ frontend/src/Movies.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table'
 import InputGroup from 'react-bootstrap/InputGroup'
 import { FormControl } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import ReactDom from 'react-dom';
+// import Popup from './PopUp.js';
+import {Modal, Button} from "react-bootstrap";
+
+function Popup(movie) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+function addRowHandlers() {
+  var table = document.getElementById("movieTable");
+    var rows = table.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+        var currentRow = table.rows[i];
+        var createClickHandler =
+            function(row)
+            {
+                return function() {
+                   var cell = row.getElementsByTagName("td")[0];
+                   var id = cell.innerHTML;
+                   alert("id:" + id);
+                  };
+            };
+
+        currentRow.onclick = createClickHandler(currentRow);
+    }
+}
 
 class Movies extends React.Component {
+
 
     //exampleMovies
     moviesR = [{title:"Harry Potter", director:"Tahir", year:'2000'}, {title:"Harry Potter 2", director:"Jay", year:'2003'}]
@@ -14,17 +59,22 @@ class Movies extends React.Component {
     state = {
         movies: [],
         searchVal: null,
+        showPopup: false,
     }
+
 
     searchMovies(e){
         //console.log(e.target.value)
         //make api call and recieve list of movies
         this.setState({movies: this.moviesR})
+        //TODO: actually search and filter movies based on title
     }
 
     render() {
         return (
             <Container align='center'>
+
+                <br />
                 <h1>Movies</h1>
                 <br />
                 <InputGroup size="lg">
@@ -37,7 +87,8 @@ class Movies extends React.Component {
                 </InputGroup>
 
                 <br />
-                <Table striped bordered hover>
+                <Popup />
+                <Table striped bordered hover id="movieTable">
                     <thead>
                         <tr>
                             <th>Title</th>
@@ -47,11 +98,13 @@ class Movies extends React.Component {
                     </thead>
                     <tbody>
                         {this.state.movies.map((movie)=>{
+                          window.onload = addRowHandlers();
                             return(
                                 <tr>
                                     <td>{movie.title}</td>
                                     <td>{movie.director}</td>
                                     <td>{movie.year}</td>
+
                                 </tr>
                             )
                         })}
