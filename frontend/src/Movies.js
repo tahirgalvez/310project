05b4d0236bfea5795520a7195f5b9f96c0f14b88
julieherrick <julieherrick@tamu.edu>
2021-11-21 14:34:01 -0600

    need to add data
    popup functions, need to work on linking row data to popup

--- frontend/src/Movies.js
+++ frontend/src/Movies.js
@@ -1,119 +1,140 @@
-import React, { useState } from 'react';
-// import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
 import { Container } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table'
+import autoBind from "react-autobind";
+import "antd/dist/antd.css";
+import { Table, Modal } from "antd";
 import InputGroup from 'react-bootstrap/InputGroup'
 import { FormControl } from 'react-bootstrap';
-import ReactDom from 'react-dom';
-// import Popup from './PopUp.js';
-import {Modal, Button} from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
-function Popup(movie) {
-  const [show, setShow] = useState(false);
+const columns = [
+  {
+    title: "Title",
+    dataIndex: "title",
+    key: "title"
+  },
+  {
+    title: "Director",
+    dataIndex: "director",
+    key: "director"
+  },
+  {
+    title: "Year",
+    dataIndex: "year",
+    key: "year"
+  }
+];
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+const data = [
+  {
+    key: "1",
+    title: "Harry Potter",
+    director: "Tahir",
+    year: "2000"
+  },
+  {
+    key: "2",
+    title: "Harry Potter 2",
+    director: "Jay",
+    year: "2001"
+  },
+  {
+    key: "3",
+    title: "Harry Potter 3",
+    director: "Tahir",
+    year: "2002"
+  },
+];
 
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      </Modal>
-    </>
-  );
-}
-
-function addRowHandlers() {
-  var table = document.getElementById("movieTable");
-    var rows = table.getElementsByTagName("tr");
-    for (let i = 0; i < rows.length; i++) {
-        var currentRow = table.rows[i];
-        var createClickHandler =
-            function(row)
-            {
-                return function() {
-                   var cell = row.getElementsByTagName("td")[0];
-                   var id = cell.innerHTML;
-                   alert("id:" + id);
-                  };
-            };
-
-        currentRow.onclick = createClickHandler(currentRow);
-    }
-}
+const getRowKey = record => {
+  return `${record.key}`;
+};
 
 class Movies extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    autoBind(this);
+    this.state = { selectedRowKey: null, visible: false };
+  }
+
+  setRowKey(record) {
+    const selectedRowKey = getRowKey(record);
+    console.log(record);
+    console.log(selectedRowKey, typeof selectedRowKey);
+    this.setState({ selectedRowKey });
+  }
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  searchMovies(e){
+    //console.log(e.target.value)
+    //make api call and recieve list of movies
+    this.setState({movies: this.moviesR})
+    //TODO: actually search and filter movies based on title
+}
+
+  render() {
+    const { selectedRowKey } = this.state;
+    console.log(selectedRowKey, typeof selectedRowKey);
 
 
-    //exampleMovies
-    moviesR = [{title:"Harry Potter", director:"Tahir", year:'2000'}, {title:"Harry Potter 2", director:"Jay", year:'2003'}]
-
-    state = {
-        movies: [],
-        searchVal: null,
-        showPopup: false,
-    }
-
-
-    searchMovies(e){
-        //console.log(e.target.value)
-        //make api call and recieve list of movies
-        this.setState({movies: this.moviesR})
-        //TODO: actually search and filter movies based on title
-    }
-
-    render() {
-        return (
-            <Container align='center'>
+    return (
+      <Container align='center'>
 
                 <br />
                 <h1>Movies</h1>
                 <br />
                 <InputGroup size="lg">
-                    <Button variant ="outline-secondary"
-                      id="movies-search-button"
-                      onClick={(e)=>this.searchMovies(e)}
-                      > Search Title
-                    </Button>
-                    <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
-                </InputGroup>
+                   <Button variant ="outline-secondary"
+                     id="movies-search-button"
+                     onClick={(e)=>this.searchMovies(e)}
+                     > Search Title
+                   </Button>
+                   <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+               </InputGroup>
 
-                <br />
-                <Popup />
-                <Table striped bordered hover id="movieTable">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Director</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.movies.map((movie)=>{
-                          window.onload = addRowHandlers();
-                            return(
-                                <tr>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.director}</td>
-                                    <td>{movie.year}</td>
+      <div className="holder">
+        <Table
+          columns={columns}
+          rowKey={record => getRowKey(record)}
+          rowClassName={record =>
+            getRowKey(record) === selectedRowKey ? "highlighted" : ""
+          }
+          onRow={record => {
+            return {
+              onClick: () => {
+                this.setState({ visible: true });
+                this.setRowKey(record);
+              }
+            };
+          }}
+          dataSource={data}
+        />
 
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>
-
-            </Container>
-        )
-    }
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>document.getElementByID("title").innerHTML</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+      </Container>
+    );
+  }
 }
 
 export default Movies;
