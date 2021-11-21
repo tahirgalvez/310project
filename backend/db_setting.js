7003a60ad2c1ef6b0d9aa436be64e44d85e7de3d
tahirgalvez <61433382+tahirgalvez@users.noreply.github.com>
2021-11-21 12:22:47 -0600

    debbuging

--- backend/db_setting.js
+++ backend/db_setting.js
@@ -15,6 +15,7 @@ const Pool = require("pg").Pool;
 const connectionString = 'postgres://oaxmrpctgtjdyk:879d07a71cedbc3f2c4e1720177eb73dce983b73369f8a03aa01d1e84d4fa0e0@ec2-3-230-149-158.compute-1.amazonaws.com:5432/dsie04961744k'
 const pool = new Pool({
   connectionString,
+  ssl: true,
 })
 
 module.exports = pool;
