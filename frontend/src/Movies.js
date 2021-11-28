2ed6c14cb48696e6f083036ee8559d41fcbb43dd
julieherrick <julieherrick@tamu.edu>
2021-11-27 21:39:15 -0600

    popup works & search
    Search filters properly except capitalization matters so still need to fix that

--- frontend/src/Movies.js
+++ frontend/src/Movies.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 import autoBind from "react-autobind";
 import "antd/dist/antd.css";
 import { Table, Modal } from "antd";
-import InputGroup from 'react-bootstrap/InputGroup'
-import { FormControl } from 'react-bootstrap';
-import {Button} from "react-bootstrap";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import ReactHtmlParser from "react-html-parser";
 
 const columns = [
   {
     title: "Title",
     dataIndex: "title",
-    key: "title"
+    key: "title",
   },
   {
     title: "Director",
     dataIndex: "director",
-    key: "director"
+    key: "director",
   },
   {
     title: "Year",
     dataIndex: "year",
-    key: "year"
-  }
+    key: "year",
+  },
 ];
 
 const data = [
@@ -31,23 +31,29 @@ const data = [
     key: "1",
     title: "Harry Potter",
     director: "Tahir",
-    year: "2000"
+    year: "2000",
   },
   {
     key: "2",
     title: "Harry Potter 2",
     director: "Jay",
-    year: "2001"
+    year: "2001",
   },
   {
     key: "3",
     title: "Harry Potter 3",
     director: "Tahir",
-    year: "2002"
+    year: "2002",
+  },
+  {
+    key: "4",
+    title: "Jaws",
+    director: "Spielberg",
+    year: "1975",
   },
 ];
 
-const getRowKey = record => {
+const getRowKey = (record) => {
   return `${record.key}`;
 };
 
@@ -55,7 +61,13 @@ class Movies extends React.Component {
   constructor(props, context) {
     super(props, context);
     autoBind(this);
-    this.state = { selectedRowKey: null, visible: false, selectedMovie: null };
+    this.state = {
+      selectedRowKey: null,
+      visible: false,
+      selectedMovie: null,
+      searchValue: "",
+      movieData: data,
+    };
   }
 
   setRowKey(record) {
@@ -63,79 +75,88 @@ class Movies extends React.Component {
     console.log(record);
     console.log(selectedRowKey, typeof selectedRowKey);
     this.setState({ selectedRowKey });
-    this.setState({record});
+    this.setState({ record });
   }
 
-  handleOk = e => {
+  handleOk = (e) => {
     console.log(e);
     this.setState({
-      visible: false
+      visible: false,
     });
   };
 
-  handleCancel = e => {
+  handleCancel = (e) => {
     console.log(e);
     this.setState({
-      visible: false
+      visible: false,
     });
   };
 
-  searchMovies(e){
-    //console.log(e.target.value)
-    //make api call and recieve list of movies
-    this.setState({movies: this.moviesR})
-    //TODO: actually search and filter movies based on title
-}
+  searchMovies(e) {
+    let newMovies = data.filter((movie) => {
+      return movie.title.indexOf(this.state.searchValue) >= 0;
+    });
+    this.setState({ movieData: newMovies });
+  }
 
   render() {
     const { selectedRowKey } = this.state;
     console.log(selectedRowKey, typeof selectedRowKey);
 
     return (
-      <Container align='center'>
+      <Container align="center">
+        <br />
+        <h1>Movies</h1>
+        <br />
+        <InputGroup size="lg">
+          <Button
+            variant="outline-secondary"
+            id="movies-search-button"
+            onClick={(e) => this.searchMovies(e)}
+          >
+            {" "}
+            Search Title
+          </Button>
+          <FormControl
+            aria-label="Large"
+            aria-describedby="inputGroup-sizing-sm"
+            value={this.state.searchValue}
+            onChange={(e) => this.setState({ searchValue: e.target.value })}
+          />
+        </InputGroup>
 
-                <br />
-                <h1>Movies</h1>
-                <br />
-                <InputGroup size="lg">
-                   <Button variant ="outline-secondary"
-                     id="movies-search-button"
-                     onClick={(e)=>this.searchMovies(e)}
-                     > Search Title
-                   </Button>
-                   <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
-               </InputGroup>
-
-      <div className="holder">
-      <p></p>
-        <Table
-          columns={columns}
-          rowKey={record => getRowKey(record)}
-          rowClassName={record =>
-            getRowKey(record) === selectedRowKey ? "highlighted" : ""
-          }
-          onRow={record => {
-            return {
-              onClick: () => {
-                this.setState({ visible: true });
-                this.setRowKey(record);
-                this.setState({selectedMovie: record.key})
-              }
-            };
-          }}
-          dataSource={data}
-        />
-
-        <Modal
-          title={ReactHtmlParser(this.state.selectedMovie)}
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
+        <div className="holder">
           <p></p>
-          <p>Some contents...</p>
-        </Modal>
-      </div>
+          <Table
+            columns={columns}
+            rowKey={(record) => getRowKey(record)}
+            rowClassName={(record) =>
+              getRowKey(record) === selectedRowKey ? "highlighted" : ""
+            }
+            onRow={(record) => {
+              return {
+                onClick: () => {
+                  this.setState({ visible: true });
+                  this.setRowKey(record);
+                  this.setState({ selectedMovie: record.key });
+                },
+              };
+            }}
+            dataSource={this.state.movieData}
+          />
+          {this.state.visible && (
+            <Modal
+              title={this.state.movieData[this.state.selectedMovie - 1].title}
+              visible={this.state.visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              <p>Title: {this.state.movieData[this.state.selectedMovie - 1].title}</p>
+              <p>Director: {this.state.movieData[this.state.selectedMovie - 1].director}</p>
+              <p>Year: {this.state.movieData[this.state.selectedMovie - 1].year}</p>
+            </Modal>
+          )}
+        </div>
       </Container>
     );
   }
