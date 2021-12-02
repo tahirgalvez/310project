8f11726e8cf11dbd867d5bdf1b705fbc5e52b728
Daniel Phan <phand972@gmail.com>
2021-12-01 18:41:00 -0600

    Updated db initialization instructions

--- backend/database.sql
+++ backend/database.sql
@@ -1,5 +1,5 @@
 -- Using postgresql
--- 
+-- Run these commands in order to initialize the database!
 
 CREATE DATABASE csce310project;
 
@@ -13,4 +13,51 @@ CREATE TABLE title(
     end_year integer,
     runtime_minutes integer,
     genres varchar(50)[]
+);
+
+CREATE TABLE rating(
+    t_const varchar(50) REFERENCES title(t_const),
+    average_rating decimal,
+    num_votes integer
+);
+
+CREATE TABLE person(
+    n_const varchar(50) PRIMARY KEY,
+    primary_name varchar(50),
+    birth_year integer,
+    death_year integer,
+    primary_profession varchar(50)[],
+    known_for_titles varchar(50)[]
+);
+
+-- CREATE TABLE crew(
+--     t_const varchar(50) REFERENCES title(t_const),
+--     directors n_const[] ELEMENT REFERENCES person(n_const),
+--     writers n_const[] ELEMENT REFERENCES person(n_const)
+-- );
+
+CREATE TABLE title_directors(
+    t_const varchar(50) REFERENCES title(t_const),
+    n_const varchar(50) REFERENCES person(n_const)
+);
+
+CREATE TABLE title_writers(
+    t_const varchar(50) REFERENCES title(t_const),
+    n_const varchar(50) REFERENCES person(n_const)
+);
+
+CREATE TABLE title_cast(
+    t_const varchar(50) REFERENCES title(t_const),
+    n_const varchar(50) REFERENCES person(n_const),
+    ordering integer,
+    category varchar(50),
+    job varchar(50),
+    characters varchar(50)
+);
+
+CREATE TABLE episode(
+    t_const varchar(50) PRIMARY KEY,
+    parent_t_const varchar(50) REFERENCES title(t_const),
+    season_number integer,
+    episode_number integer
 );
