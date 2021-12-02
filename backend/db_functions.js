f099e6b4d1212aba8756f1ce2b267c1b2d008015
Daniel Phan <phand972@gmail.com>
2021-12-01 19:32:20 -0600

    Updated documentation to title queries

--- backend/db_functions.js
+++ backend/db_functions.js
@@ -2,15 +2,24 @@ console.log("db_functions.js loading");
 
 const dbf = class DBFunctions{
     // Title Queries
-    getAllTitles() {
-        return "SELECT * FROM title";
-    }
-
     // Basic CRUD for titles
-    insertTitle(tconst, titletype, primarytitle, originaltitle, isadult, startyear, endyear, runtimeminutes, genres) {
+    /**
+     * Inserts a media into the title table.
+     * @param {string} tconst string - Primary key of the title table
+     * @param {string} titleType string - The type/format of the title (e.g. movie, short, tvseries, tvepisode, video, etc)
+     * @param {string} primaryTitle string - The more popular title / the title used by the filmmakers on promotional materials at the point of release
+     * @param {string} originalTitle string - Original title, in the original language
+     * @param {boolean} isAdult boolean - True for adult titles, false otherwise
+     * @param {number} startYear number - Represents the release year of a title. In the case of TV Series, it is the series start year
+     * @param {number} endYear number - TV Series end year. Null for all other title types
+     * @param {number} runTimeMinutes number - Primary runtime of the title, in minutes
+     * @param {string[]} genres string[] - Includes up to three genres associated with the title
+     * @returns {string} string - SQL query command to insert a media to the title table
+     */
+    insertTitle(tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runTimeMinutes, genres) {
         var query = `
 INSERT INTO title(t_const, title_type, primary_title, original_title, is_adult, start_year, end_year, runtime_minutes, genres)
-    VALUES ('${tconst}', '${titletype}', '${primarytitle}', '${originaltitle}', '${isadult}', '${startyear}', '${endyear}', ${runtimeminutes}, '{`;
+    VALUES ('${tconst}', '${titleType}', '${primaryTitle}', '${originalTitle}', '${isAdult}', '${startYear}', '${endYear}', ${runTimeMinutes}, '{`;
         for (let i = 0; i < genres.length; i++) {
             if (i != genres.length - 1) {
                 query += `"${genres[i]}",`;
@@ -23,6 +32,11 @@ INSERT INTO title(t_const, title_type, primary_title, original_title, is_adult, 
         return query;
     }
 
+    /**
+     * Gets a media from the title table.
+     * @param {string} tconst 
+     * @returns {string} string - SQL query command to get a title
+     */
     getTitle(tconst) {
         var query = `
 SELECT * FROM title 
@@ -32,18 +46,31 @@ SELECT * FROM title
         return query;
     }
 
-    updateTitle(tconst, titletype, primarytitle, originaltitle, isadult, startyear, endyear, runtimeminutes, genres) {
+    /**
+     * Update a media from the title table.
+     * @param {string} tconst string - Primary key of the title table
+     * @param {string} titleType string - The type/format of the title (e.g. movie, short, tvseries, tvepisode, video, etc)
+     * @param {string} primaryTitle string - The more popular title / the title used by the filmmakers on promotional materials at the point of release
+     * @param {string} originalTitle string - Original title, in the original language
+     * @param {boolean} isAdult boolean - True for adult titles, false otherwise
+     * @param {number} startYear number - Represents the release year of a title. In the case of TV Series, it is the series start year
+     * @param {number} endYear number - TV Series end year. Null for all other title types
+     * @param {number} runTimeMinutes number - Primary runtime of the title, in minutes
+     * @param {string[]} genres string[] - Includes up to three genres associated with the title
+     * @returns {string} string - SQL query command to update the title table
+     */
+    updateTitle(tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runTimeMinutes, genres) {
         var query = `
 UPDATE title
 SET t_const = '${tconst}',
-    title_type = '${titletype}',
-    primary_title = '${primarytitle}',
-    original_title = '${originaltitle}',
-    is_adult = ${isadult},
-    start_year = ${startyear},
-    end_year = ${endyear},
-    runtime_minutes = ${runtimeminutes},
-    genres = '{`
+    title_type = '${titleType}',
+    primary_title = '${primaryTitle}',
+    original_title = '${originalTitle}',
+    is_adult = ${isAdult},
+    start_year = ${startYear},
+    end_year = ${endYear},
+    runtime_minutes = ${runTimeMinutes},
+    genres = '{`;
         for (let i = 0; i < genres.length; i++) {
             if (i != genres.length - 1) {
                 query += `"${genres[i]}", `;
@@ -58,6 +85,11 @@ WHERE title.t_const = '${tconst}';`;
         return query;
     }
 
+    /**
+     * Delete a media from the title table given its t_const.
+     * @param {string} tconst string - Primary key to the title table
+     * @returns {string} string - SQL query command to delete a media from the title table
+     */
     deleteTitle(tconst) {
         var query = `
 DELETE FROM title
@@ -68,21 +100,37 @@ DELETE FROM title
     }
 
     // Advanced Search
-    searchTitle(title=null, genre=null, minRating=null, maxRating=null, page=1, itemsPerPage=50) {
+    /**
+     * Advanced Search. Use only relevant parameters, the rest can be set to null.
+     * @param {string} title string - Title of the media
+     * @param {string[]} titleType string[] - Types of media
+     * @param {boolean} isAdult boolean - If the media is for adults. null represents both.
+     * @param {number} minYear number - Minimum year aired / release of the media.
+     * @param {number} maxYear number - Max year aired / release of the media
+     * @param {number} minRunTimeMinutes number - Minimum run time minutes of the media
+     * @param {number} maxRunTimeMinutes number - Max run time minutes of the media
+     * @param {number} minRating number - Minimum rating (inclusive)
+     * @param {number} maxRating number - Max rating (inclusive)
+     * @param {string[]} genres string[] - Genres of the media (i.e. romance, drama, action...)
+     * @param {number} page number - Page of UI to display
+     * @param {number} itemsPerPage number - Amount of items per page
+     * @returns {string} string - SQL query command to search for media in the title table
+     */
+    searchTitle(title=null, titleTypes=null, isAdult=null, minYear=null, maxYear=null, minRunTimeMinutes=null, maxRunTimeMinutes=null, minRating=null, maxRating=null, genres=null, page=1, itemsPerPage=50) {
 
         if (title == null) {
             title = ''
         }
-        
-        console.log(title + " " + genre + " " + minRating + " " + maxRating + " " + page + " " + itemsPerPage);
+        //console.log(title + " " + genre + " " + minRating + " " + maxRating + " " + page + " " + itemsPerPage);
 
 
         var query = `
 SELECT * FROM title, rating
     WHERE (title.primary_title LIKE \'%${title}%\' OR title.original_title LIKE \'%${title}%\') `;
 
-        if (genre != null) {
-            query += `AND ('${genre}' = ANY(title.genres) `;
+        // TODO
+        if (genres != null) {
+            query += `AND ('${genres}' = ANY(title.genres) `;
         }
 
         if (minRating != null && maxRating != null) {
