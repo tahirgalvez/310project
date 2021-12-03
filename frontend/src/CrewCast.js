c5b53813893827d636347b55d1970b7c478493f3
julieherrick <julieherrick@tamu.edu>
2021-12-03 11:15:40 -0600

    update show and cast & crew table

--- frontend/src/CrewCast.js
+++ frontend/src/CrewCast.js
@@ -1,65 +1,164 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table'
-import InputGroup from 'react-bootstrap/InputGroup'
-import { FormControl } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import { Container } from "react-bootstrap";
+import autoBind from "react-autobind";
+import "antd/dist/antd.css";
+import { Table, Modal } from "antd";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Primary Profession",
+    dataIndex: "profession",
+    key: "profession",
+  },
+  {
+    title: "Birth Year",
+    dataIndex: "dob",
+    key: "dob",
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    name: "Daniel Radcliffe",
+    profession: "Actor",
+    dob: "1989",
+  },
+  {
+    key: "2",
+    name: "Emma Watson",
+    profession: "Actress",
+    dob: "1990",
+  },
+  {
+    key: "3",
+    name: "Rupert Grint",
+    profession: "Actor",
+    dob: "1988",
+  },
+  {
+    key: "4",
+    name: "Tom Felton",
+    profession: "Actor",
+    dob: "1987",
+  },
+];
+
+const getRowKey = (record) => {
+  return `${record.key}`;
+};
 
 class CrewCast extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    autoBind(this);
+    this.state = {
+      selectedRowKey: null,
+      visible: false,
+      selectedPerson: null,
+      searchValue: "",
+      personData: data,
+    };
+  }
 
-    peopleR = [{ name: "Harry Potter", title: "Tahir", dob: '2000' }, { name: "Harry Potter 2", title: "Jay", dob: '2003' }]
+  setRowKey(record) {
+    const selectedRowKey = getRowKey(record);
+    console.log(record);
+    console.log(selectedRowKey, typeof selectedRowKey);
+    this.setState({ selectedRowKey });
+    this.setState({ record });
+  }
 
-    state = {
-        people: [],
-        searchVal: null,
-    }
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
-    searchPeople(e) {
-        //console.log(e.target.value)
-        //make api call and recieve list of movies
-        this.setState({ people: this.peopleR })
-    }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
-    render() {
-        return (
-            <Container align='center'>
-                <h1>Crew & Cast</h1>
-                <br />
-                <InputGroup size="lg">
-                    <Button variant="outline-secondary"
-                        id="movies-search-button"
-                        onClick={(e) => this.searchPeople(e)}
-                    > Search Name
-                    </Button>
-                    <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
-                </InputGroup>
+  searchCrewCast(e) {
+    let newCrewCast = data.filter((person) => {
+      return person.name.toLowerCase().indexOf(this.state.searchValue.toLowerCase()) >= 0;
+    });
+    this.setState({ personData: newCrewCast });
+  }
 
-                <br />
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Title</th>
-                            <th>Date of Birth</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.people.map((p) => {
-                            return (
-                                <tr>    
-                                    <td>{p.name}</td>
-                                    <td>{p.title}</td>
-                                    <td>{p.dob}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>
+  render() {
+    const { selectedRowKey } = this.state;
+    console.log(selectedRowKey, typeof selectedRowKey);
 
-            </Container>
-        )
-    }
+    return (
+      <Container align="center">
+        <br />
+        <h1>CrewCast</h1>
+        <br />
+        <InputGroup size="lg">
+          <Button
+            variant="outline-secondary"
+            id="crew-cast-search-button"
+            onClick={(e) => this.searchCrewCast(e)}
+          >
+            {" "}
+            Search Name
+          </Button>
+          <FormControl
+            aria-label="Large"
+            aria-describedby="inputGroup-sizing-sm"
+            value={this.state.searchValue}
+            onChange={(e) => this.setState({ searchValue: e.target.value })}
+          />
+        </InputGroup>
+
+        <div className="holder">
+          <p></p>
+          <Table
+            columns={columns}
+            rowKey={(record) => getRowKey(record)}
+            rowClassName={(record) =>
+              getRowKey(record) === selectedRowKey ? "highlighted" : ""
+            }
+            onRow={(record) => {
+              return {
+                onClick: () => {
+                  this.setState({ visible: true });
+                  this.setRowKey(record);
+                  this.setState({ selectedPerson: record });
+                },
+              };
+            }}
+            dataSource={this.state.personData}
+          />
+          {this.state.visible && (
+            <Modal
+              title={this.state.selectedPerson.name}
+              visible={this.state.visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              <p>Name: {this.state.selectedPerson.name}</p>
+              <p>Primary Profession: {this.state.selectedPerson.profession}</p>
+              <p>Birth Year: {this.state.selectedPerson.dob}</p>
+            </Modal>
+          )}
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default CrewCast;
--- frontend/src/Shows.js
+++ frontend/src/Shows.js
@@ -1,65 +1,158 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table'
-import InputGroup from 'react-bootstrap/InputGroup'
-import { FormControl } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import { Container } from "react-bootstrap";
+import autoBind from "react-autobind";
+import "antd/dist/antd.css";
+import { Table, Modal } from "antd";
+import InputGroup from "react-bootstrap/InputGroup";
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+const columns = [
+  {
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
+  },
+  {
+    title: "Director",
+    dataIndex: "director",
+    key: "director",
+  },
+  {
+    title: "Year",
+    dataIndex: "year",
+    key: "year",
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    title: "The Office",
+    director: "Tahir",
+    year: "2000",
+  },
+  {
+    key: "2",
+    title: "Parks and Recreation",
+    director: "Jay",
+    year: "2001",
+  },
+  {
+    key: "3",
+    title: "New Girl",
+    director: "Tahir",
+    year: "2002",
+  },
+];
+
+const getRowKey = (record) => {
+  return `${record.key}`;
+};
 
 class Shows extends React.Component {
-    //exampleMovies
-    showsR = [{ title: "Harry Potter Show", director: "Tahir", year: '2000' }, { title: "Harry Potter Show 2", director: "Jay", year: '2003' }]
+  constructor(props, context) {
+    super(props, context);
+    autoBind(this);
+    this.state = {
+      selectedRowKey: null,
+      visible: false,
+      selectedShow: null,
+      searchValue: "",
+      showData: data,
+    };
+  }
 
-    state = {
-        shows: [],
-        searchVal: null,
-    }
+  setRowKey(record) {
+    const selectedRowKey = getRowKey(record);
+    console.log(record);
+    console.log(selectedRowKey, typeof selectedRowKey);
+    this.setState({ selectedRowKey });
+    this.setState({ record });
+  }
 
-    searchShows(e) {
-        //console.log(e.target.value)
-        //make api call and recieve list of movies
-        this.setState({ shows: this.showsR })
-    }
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
-    render() {
-        return (
-            <Container align='center'>
-                <h1>Shows</h1>
-                <br />
-                <InputGroup size="lg">
-                    <Button variant="outline-secondary"
-                        id="movies-search-button"
-                        onClick={(e) => this.searchShows(e)}
-                    > Search Title
-                    </Button>
-                    <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
-                </InputGroup>
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
-                <br />
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Director</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.shows.map((shows) => {
-                            return (
-                                <tr>
-                                    <td>{shows.title}</td>
-                                    <td>{shows.director}</td>
-                                    <td>{shows.year}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>
+  searchShows(e) {
+    let newShows = data.filter((show) => {
+      return show.title.toLowerCase().indexOf(this.state.searchValue.toLowerCase()) >= 0;
+    });
+    this.setState({ showData: newShows });
+  }
 
-            </Container>
-        )
-    }
+  render() {
+    const { selectedRowKey } = this.state;
+    console.log(selectedRowKey, typeof selectedRowKey);
+
+    return (
+      <Container align="center">
+        <br />
+        <h1>Shows</h1>
+        <br />
+        <InputGroup size="lg">
+          <Button
+            variant="outline-secondary"
+            id="shows-search-button"
+            onClick={(e) => this.searchShows(e)}
+          >
+            {" "}
+            Search Title
+          </Button>
+          <FormControl
+            aria-label="Large"
+            aria-describedby="inputGroup-sizing-sm"
+            value={this.state.searchValue}
+            onChange={(e) => this.setState({ searchValue: e.target.value })}
+          />
+        </InputGroup>
+
+        <div className="holder">
+          <p></p>
+          <Table
+            columns={columns}
+            rowKey={(record) => getRowKey(record)}
+            rowClassName={(record) =>
+              getRowKey(record) === selectedRowKey ? "highlighted" : ""
+            }
+            onRow={(record) => {
+              return {
+                onClick: () => {
+                  this.setState({ visible: true });
+                  this.setRowKey(record);
+                  this.setState({ selectedShow: record });
+                },
+              };
+            }}
+            dataSource={this.state.showData}
+          />
+          {this.state.visible && (
+            <Modal
+              title={this.state.selectedShow.title}
+              visible={this.state.visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              <p>Title: {this.state.selectedShow.title}</p>
+              <p>Director: {this.state.selectedShow.director}</p>
+              <p>Year: {this.state.selectedShow.year}</p>
+            </Modal>
+          )}
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default Shows;
